428fde4fcf2e1edd7ef97362f85abf1e1bfb60dc
Natachat Teecharcharn <nteechar@Natachats-MacBook-Air.local>
2024-12-18 12:32:45 +0700

    Done Ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -5,7 +5,11 @@ int main(int argc, char **argv)
     (void) argc;
     (void) argv;
 
-    Zombie  zombie("Alex");
+    Zombie *zombie = newZombie("newZombie");
+    zombie->announce();
+    delete zombie;
 
-    zombie.announce();
+    randomChump("randomChump");
+
+    return (0);
 }
