7034d108870084a46fd4b93ceabfdc4daa71b727
Natachat Teecharcharn <techazuza@gmail.com>
2024-12-21 14:03:21 +0700

    Ex05: Implement basic Harl functionalities

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -1,26 +1,54 @@
+#include <iostream>
 #include "Harl.hpp"
 
 void Harl::complain(std::string level)
 {
-
+	if (level == "DEBUG")
+		Harl::debug();
+	else if (level == "INFO")
+		Harl::info();
+	else if (level == "WARNING")
+		Harl::warning();
+	else if (level == "ERROR")
+		Harl::error();
+	else
+		std::cout << "Wait. I confused myself!" << std::endl;
 }
 
 void Harl::debug(void)
 {
-
+	std::cout
+		<< "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger."
+		<< std::endl
+		<< "I really do!"
+		<< std::endl;
 }
 
 void Harl::info(void)
 {
-
+	std::cout
+		<< "I cannot believe adding extra bacon costs more money."
+		<< std::endl
+		<< "You didn’t put enough bacon in my burger!"
+		<< std::endl
+		<< "If you did, I wouldn’t be asking for more!"
+		<< std::endl;
 }
 
 void Harl::warning(void)
 {
-
+	std::cout
+		<< "I think I deserve to have some extra bacon for free."
+		<< std::endl
+		<< "I’ve been coming for years whereas you started working here since last month."
+		<< std::endl;
 }
 
 void Harl::error(void)
 {
-
+	std::cout
+		<< "This is unacceptable!"
+		<< std::endl
+		<< "I want to speak to the manager now."
+		<< std::endl;
 }
--- ex05/main.cpp
+++ ex05/main.cpp
@@ -1,10 +1,25 @@
+#include <iostream>
 #include "Harl.hpp"
 
-int main(int argc, char **argv)
+int main(void)
 {
-    (void) argc; (void) argv;
+    Harl harl;
 
+    std::cout << "(DEBUG)" << std::endl;
+    harl.complain("DEBUG");
+    std::cout << std::endl;
 
+    std::cout << "(INFO)" << std::endl;
+    harl.complain("INFO");
+    std::cout << std::endl;
+
+    std::cout << "(WARNING)" << std::endl;
+    harl.complain("WARNING");
+    std::cout << std::endl;
+
+    std::cout << "(ERROR)" << std::endl;
+    harl.complain("ERROR");
+    std::cout << std::endl;
 
     return (0);
 }
